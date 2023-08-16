e79594d489f212fd0978254f55c5a36b8c5461f1
Luis_Olivarria <luis.olivarria@unilane.mx>
2023-08-16 16:52:55 -0700

    integracion de correo de nuevo pedido, recuperacion de contraseña

---
+++ generated/code/Mageplaza/SocialLogin/Controller/Popup/Forgot/Interceptor.php
@@ -0,0 +1,70 @@
+<?php
+namespace Mageplaza\SocialLogin\Controller\Popup\Forgot;
+
+/**
+ * Interceptor class for @see \Mageplaza\SocialLogin\Controller\Popup\Forgot
+ */
+class Interceptor extends \Mageplaza\SocialLogin\Controller\Popup\Forgot implements \Magento\Framework\Interception\InterceptorInterface
+{
+    use \Magento\Framework\Interception\Interceptor;
+
+    public function __construct(\Magento\Framework\App\Action\Context $context, \Magento\Customer\Model\Session $customerSession, \Magento\Customer\Api\AccountManagementInterface $customerAccountManagement, \Magento\Framework\Escaper $escaper, \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory, \Magento\Captcha\Helper\Data $captchaHelper, \Mageplaza\SocialLogin\Helper\Data $socialHelper)
+    {
+        $this->___init();
+        parent::__construct($context, $customerSession, $customerAccountManagement, $escaper, $resultJsonFactory, $captchaHelper, $socialHelper);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function checkCaptcha()
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'checkCaptcha');
+        return $pluginInfo ? $this->___callPlugins('checkCaptcha', func_get_args(), $pluginInfo) : parent::checkCaptcha();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute()
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'execute');
+        return $pluginInfo ? $this->___callPlugins('execute', func_get_args(), $pluginInfo) : parent::execute();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function dispatch(\Magento\Framework\App\RequestInterface $request)
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'dispatch');
+        return $pluginInfo ? $this->___callPlugins('dispatch', func_get_args(), $pluginInfo) : parent::dispatch($request);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getActionFlag()
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'getActionFlag');
+        return $pluginInfo ? $this->___callPlugins('getActionFlag', func_get_args(), $pluginInfo) : parent::getActionFlag();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRequest()
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'getRequest');
+        return $pluginInfo ? $this->___callPlugins('getRequest', func_get_args(), $pluginInfo) : parent::getRequest();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getResponse()
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'getResponse');
+        return $pluginInfo ? $this->___callPlugins('getResponse', func_get_args(), $pluginInfo) : parent::getResponse();
+    }
+}
--- pub/static/frontend/Smartwave/porto/en_US/Mageplaza_Osc/js/model/shipping-save-processor/checkout.js
+++ pub/static/frontend/Smartwave/porto/en_US/Mageplaza_Osc/js/model/shipping-save-processor/checkout.js
@@ -49,12 +49,8 @@ define(
 
         return {
             saveShippingInformation: function () {
-                var payload,
-                    addressInformation = {},
-                    additionInformation = oscData.getData();
-                if (window.checkoutConfig.oscConfig.giftMessageOptions.isOrderLevelGiftOptionsEnabled) {
-                    additionInformation.giftMessage = this.saveGiftMessage();
-                }
+                var payload, addressInformation = {}, additionInformation = oscData.getData();
+               
                 if (!quote.billingAddress()) {
                     selectBillingAddressAction(quote.shippingAddress());
                 }
@@ -73,29 +69,11 @@ define(
                     addressInformation = {
                         billing_address: quote.billingAddress()
                     };
-                }
-
-                var customAttributes = {};
-                if (_.isObject(quote.billingAddress().customAttributes)) {
-                    _.each(quote.billingAddress().customAttributes, function (attribute, key) {
-                        if (_.isArray(attribute)) {
-                            customAttributes[key] = attribute.join(',')
-                        } else if (_.isString(attribute) || _.isNumber(attribute)) {
-                            customAttributes[key] = attribute
-                        } else if (_.isObject(attribute)) {
-                            customAttributes[attribute.attribute_code] = attribute.value
-                        }
-                    });
-                }
-                var extension_attributes = addressInformation.shipping_address.extension_attributes,
-                    extensionAttributes =   addressInformation.shipping_address.extensionAttributes;
-                addressInformation.shipping_address.extension_attributes=$.extend(extension_attributes, extensionAttributes);
+                }               
                 payload = {
-                    addressInformation: addressInformation,
-                    customerAttributes: customAttributes,
+                    addressInformation: addressInformation,                    
                     additionInformation: additionInformation
                 };
-
                 this.payloadExtender(payload);
 
                 fullScreenLoader.startLoader();
@@ -112,37 +90,12 @@ define(
                         fullScreenLoader.stopLoader();
                     }
                 );
-            },
-
-            saveGiftMessage: function () {
-                var giftMessage = {};
-                if (!$("#osc-gift-message").is(":checked")) $('.gift-options-content').find('input:text,textarea').val('');
-                giftMessage.sender = $("#gift-message-whole-from").val();
-                giftMessage.recipient = $("#gift-message-whole-to").val();
-                giftMessage.message = $("#gift-message-whole-message").val();
-                return JSON.stringify(giftMessage);
-            },
+            },         
 
             payloadExtender: function (payload) {
                 if (!payload.addressInformation.hasOwnProperty('shipping_address')) {
                     return payload;
-                }
-
-                var deliveryData = {
-                    mp_delivery_date: $('#mp-delivery-date').val(),
-                    mp_delivery_time: $('#mp-delivery-time').val(),
-                    mp_house_security_code: $('#mp-house-security-code').val(),
-                    mp_delivery_comment: $('#mp-delivery-comment').val()
-                };
-
-                if (!payload.addressInformation.shipping_address.hasOwnProperty('extension_attributes')) {
-                    payload.addressInformation.shipping_address.extension_attributes = {};
-                }
-
-                payload.addressInformation.shipping_address.extension_attributes = $.extend(
-                    payload.addressInformation.shipping_address.extension_attributes,
-                    deliveryData
-                )
+                }                
             }
         };
     }
