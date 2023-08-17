8b25a2e6add504185ff976f3ba90cde0463d0e2f
Luis_Olivarria <luis.olivarria@unilane.mx>
2023-08-17 11:56:27 -0700

    archivos que no se subieron

--- pub/static/frontend/Smartwave/porto/en_US/Mageplaza_Osc/js/model/shipping-save-processor/checkout.js
+++ pub/static/frontend/Smartwave/porto/en_US/Mageplaza_Osc/js/model/shipping-save-processor/checkout.js
@@ -52,9 +52,7 @@ define(
                 var payload,
                     addressInformation = {},
                     additionInformation = oscData.getData();
-                if (window.checkoutConfig.oscConfig.giftMessageOptions.isOrderLevelGiftOptionsEnabled) {
-                    additionInformation.giftMessage = this.saveGiftMessage();
-                }
+              
                 if (!quote.billingAddress()) {
                     selectBillingAddressAction(quote.shippingAddress());
                 }
@@ -74,25 +72,10 @@ define(
                         billing_address: quote.billingAddress()
                     };
                 }
-
-                var customAttributes = {};
-                if (_.isObject(quote.billingAddress().customAttributes)) {
-                    _.each(quote.billingAddress().customAttributes, function (attribute, key) {
-                        if (_.isArray(attribute)) {
-                            customAttributes[key] = attribute.join(',')
-                        } else if (_.isString(attribute) || _.isNumber(attribute)) {
-                            customAttributes[key] = attribute
-                        } else if (_.isObject(attribute)) {
-                            customAttributes[attribute.attribute_code] = attribute.value
-                        }
-                    });
-                }
-                var extension_attributes = addressInformation.shipping_address.extension_attributes,
-                    extensionAttributes =   addressInformation.shipping_address.extensionAttributes;
-                addressInformation.shipping_address.extension_attributes=$.extend(extension_attributes, extensionAttributes);
+              
+               
                 payload = {
-                    addressInformation: addressInformation,
-                    customerAttributes: customAttributes,
+                    addressInformation: addressInformation,                    
                     additionInformation: additionInformation
                 };
 
@@ -112,37 +95,12 @@ define(
                         fullScreenLoader.stopLoader();
                     }
                 );
-            },
-
-            saveGiftMessage: function () {
-                var giftMessage = {};
-                if (!$("#osc-gift-message").is(":checked")) $('.gift-options-content').find('input:text,textarea').val('');
-                giftMessage.sender = $("#gift-message-whole-from").val();
-                giftMessage.recipient = $("#gift-message-whole-to").val();
-                giftMessage.message = $("#gift-message-whole-message").val();
-                return JSON.stringify(giftMessage);
-            },
+            },  
 
             payloadExtender: function (payload) {
                 if (!payload.addressInformation.hasOwnProperty('shipping_address')) {
                     return payload;
-                }
-
-                var deliveryData = {
-                    mp_delivery_date: $('#mp-delivery-date').val(),
-                    mp_delivery_time: $('#mp-delivery-time').val(),
-                    mp_house_security_code: $('#mp-house-security-code').val(),
-                    mp_delivery_comment: $('#mp-delivery-comment').val()
-                };
-
-                if (!payload.addressInformation.shipping_address.hasOwnProperty('extension_attributes')) {
-                    payload.addressInformation.shipping_address.extension_attributes = {};
-                }
-
-                payload.addressInformation.shipping_address.extension_attributes = $.extend(
-                    payload.addressInformation.shipping_address.extension_attributes,
-                    deliveryData
-                )
+                }              
             }
         };
     }
